d2cdb3817bb24a0b8f87494fac70d606b2a396cd
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-05-29 13:16:48 +0200

    Add logic for hamburger menu & images Layout improve

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+const bodyElm = document.querySelector('body');
+const hamburgerElm = document.querySelector('.hamburger-menu');
+const navElm = document.querySelector('.nav');
 
+const toggleNav = (e) => {
+  e.stopPropagation();
+  navElm.classList.toggle('nav__open');
+};
+
+hamburgerElm.addEventListener('click', toggleNav);
+
+bodyElm.addEventListener('click', () => {
+  navElm.classList.remove('nav__open');
+});
