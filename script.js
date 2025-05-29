80d5f6850dfce61c4ed890b44ac5b64dffd5a583
Lucie Vadkerti <63465954+Lucie-git@users.noreply.github.com>
2025-05-29 15:41:51 +0200

    About pages layout ready

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const bodyElm = document.querySelector('body');
+const logoElm = document.querySelector('.logo');
 const hamburgerElm = document.querySelector('.hamburger-menu');
 const navElm = document.querySelector('.nav');
 
@@ -7,8 +8,15 @@ const toggleNav = (e) => {
   navElm.classList.toggle('nav__open');
 };
 
+const homeRedirect = (e) => {
+  e.stopPropagation();
+  window.location.href = 'O_projektu.html';
+};
+
 hamburgerElm.addEventListener('click', toggleNav);
 
 bodyElm.addEventListener('click', () => {
   navElm.classList.remove('nav__open');
 });
+
+logoElm.addEventListener('click', homeRedirect);
